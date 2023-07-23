684caa327838662d7ce3ee730e4dd729e3875100
TheFastTheHackerTheTilted <spamjunkemir@gmail.com>
2023-07-23 15:10:31 +0200

    Updated animation

--- bp/bp_clickcounter.js
+++ bp/bp_clickcounter.js
@@ -4,7 +4,7 @@ function click_increment(){
 	let count_text = document.getElementById("id_count");
 	count++;
 	count_text.innerText = count;
-
+	count_text.classList.remove("animatedDiv");
 	count_text.classList.add("animatedDiv");
   	setTimeout(function() {
 	    count_text.classList.remove("animatedDiv");
