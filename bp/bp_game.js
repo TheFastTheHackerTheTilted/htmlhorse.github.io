05a5cc5525ca371c4d4d496bcb665af5f1bd8aa6
TheFastTheHackerTheTilted <spamjunkemir@gmail.com>
2023-08-06 21:53:21 +0200

    game page updated, example items added, switch between overview and inv added

--- bp/bp_game.js
+++ bp/bp_game.js
@@ -15,3 +15,61 @@ function fancyWriteLog(text, colorcode){
 	logs.innerHTML = '<span style="color:'+colorcode+';">'+"<br><"+line+">"+text+'</span>'+logs.innerHTML;
 	line++;
 }
+
+
+
+/*
+	**Items have stats**
+
+	Name
+	Type
+	Rarity
+	extra{
+		special = true/false if other stats exist!
+		extraHealth
+		extraEnergy
+		physicalDefense
+		magicalDefense
+		enviromentalDefense
+		physicalDamage
+		magicalDamage
+		lifeStealRate
+		critChance
+		critDamageMultiplier
+		element
+		bonusLife
+	}
+*/
+function ItemObject(name,type,rarity,extra){
+	this.name = name;
+	this.type = type;
+	this.rarity = rarity;
+	this.extra = extra;
+}
+function randomItemGenerator(){
+
+}
+
+function testItemCreation(){
+	const testItem = new ItemObject("Fire Sword","Type.SWORD","Rarity.EPIC",{special:false});
+    const testItem2 = new ItemObject("Water Sword","Type.SWORD","Rarity.RARE", {special:true, element: 'fire', extraHealth: 15});
+    let itemList = [testItem,testItem2];
+    console.log(itemList);
+}
+
+function showInventory(){
+	let showinv= document.getElementById("id_inventory");
+	let showovw= document.getElementById("id_overview");
+	showinv.style.display = "block";
+	showovw.style.display = "none";
+
+
+
+}
+
+function showOverview(){
+	let showinv= document.getElementById("id_inventory");
+	let showovw= document.getElementById("id_overview");
+	showinv.style.display = "none";
+	showovw.style.display = "block";
+}
