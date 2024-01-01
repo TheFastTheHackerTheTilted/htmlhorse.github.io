cff28466098d5b92640e5f497a26c20441de6ae9
TheFastTheHackerTheTilted <spamjunkemir@gmail.com>
2024-01-01 16:25:19 +0100

    links are updated, .html s are removed

--- main.js
+++ main.js
@@ -18,23 +18,23 @@ function errorPage(){
 function addAllBps(){
 
 
-    addBp("./bp/bp_home.html","bp_home","For simple homepage.")
-    addBp("./bp/bp_bp.html","bp_blueprints","For blueprints page, ironic.")
-    addBp("./bp/bp_backgroundvideo.html","bp_backgroundvideo","For pages with video background.")
-    addBp("./bp/bp_randomizer.html","bp_randomizer","For seed based random number generator.")
-    addBp("./bp/bp_blackjack.html","bp_blackjack","For blackjack gamble game.")
-    addBp("./bp/bp_sf6.html","bp_sf6","Dedicated for street fighter 6.")
-    addBp("./bp/bp_clickcounter.html","bp_clickcounter","For click counter.")
-    addBp("./bp/bp_wiki_mixedup.html","bp_wiki_mixedup","For the mod Mixed-up.")
-    addBp("./bp/bp_designshop.html","bp_designshop","For the design of a shopping page.")
-	addBp("./bp/bp_game.html","bp_game","For the interactive game.")
-	addBp("./bp/bp_shipwrecked.html","bp_shipwrecked","For example game shipwrecked where you think carefully and survive");
-	addBp("./bp/bp_space.html","bp_space","For simulation of space traveling");
-	addBp("./bp/bp_givethephone.html","bp_givethephone","For the ' giving the phone to x' game");
-	addBp("./bp/bp_jackpot.html","bp_jackpot","For the jackpot simulation game");
-	addBp("./bp/bp_potionmake.html","bp_potionmake","For the potion crafting game");
-	addBp("./bp/bp_popupnotif.html","bp_popupnotif","For the example popup notification");
-	addBp("./bp/bp_vegetablereminder/vegetable_reminder.html","bp_vegetablereminder","For the project of vegetable reminder app");
+    addBp("./bp/bp_home","bp_home","For simple homepage.")
+    addBp("./bp/bp_bp","bp_blueprints","For blueprints page, ironic.")
+    addBp("./bp/bp_backgroundvideo","bp_backgroundvideo","For pages with video background.")
+    addBp("./bp/bp_randomizer","bp_randomizer","For seed based random number generator.")
+    addBp("./bp/bp_blackjack","bp_blackjack","For blackjack gamble game.")
+    addBp("./bp/bp_sf6","bp_sf6","Dedicated for street fighter 6.")
+    addBp("./bp/bp_clickcounter","bp_clickcounter","For click counter.")
+    addBp("./bp/bp_wiki_mixedup","bp_wiki_mixedup","For the mod Mixed-up.")
+    addBp("./bp/bp_designshop","bp_designshop","For the design of a shopping page.")
+	addBp("./bp/bp_game","bp_game","For the interactive game.")
+	addBp("./bp/bp_shipwrecked","bp_shipwrecked","For example game shipwrecked where you think carefully and survive");
+	addBp("./bp/bp_space","bp_space","For simulation of space traveling");
+	addBp("./bp/bp_givethephone","bp_givethephone","For the ' giving the phone to x' game");
+	addBp("./bp/bp_jackpot","bp_jackpot","For the jackpot simulation game");
+	addBp("./bp/bp_potionmake","bp_potionmake","For the potion crafting game");
+	addBp("./bp/bp_popupnotif","bp_popupnotif","For the example popup notification");
+	addBp("./bp/bp_vegetablereminder/vegetable_reminder","bp_vegetablereminder","For the project of vegetable reminder app");
 
 }
 
